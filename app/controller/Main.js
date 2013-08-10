6010b8909709415795a7b4d672e3986112c30c02
jose armando gonzalez lopez <iam@armando.mx>
2013-08-10 11:37:16 -0500

    starter kit generado con sencha command para extjs4

---
+++ app/controller/Main.js
@@ -0,0 +1,3 @@
+Ext.define('default.controller.Main', {
+    extend: 'Ext.app.Controller'
+});
---
+++ app/view/Main.js
@@ -0,0 +1,4 @@
+Ext.define("default.view.Main", {
+    extend: 'Ext.Component',
+    html: 'Hello, World!!'
+});
